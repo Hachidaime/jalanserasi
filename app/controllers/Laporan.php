6319f74801137ee6c6366c47bc98a1c71b1503ba
Hachidaime <cold.kid40@gmail.com>
2020-11-15 06:22:34 +0700

    Update Laporan: fix bugs

--- app/controllers/Laporan.php
+++ app/controllers/Laporan.php
@@ -14,7 +14,7 @@ class Laporan extends Controller
             case 'search':
                 $this->Dd1Search();
                 break;
-            case 'pdf';
+            case 'pdf':
                 $this->Dd1DownloadPdf();
                 break;
             case 'xlsx':
@@ -28,26 +28,26 @@ class Laporan extends Controller
 
     private function Dd1DefaultOld()
     {
-        Functions::setTitle("Laporan DD1");
+        Functions::setTitle('Laporan DD1');
 
         // TODO: Menampilkan Table
         $data['thead'] = $this->my_model->getDd1Thead();
         $data['data'] = Functions::makeTableData(['show-export' => 'true']);
         $data['search'] = false;
-        $data['url'] = BASE_URL . "/Laporan/dd1/search";
+        $data['url'] = BASE_URL . '/Laporan/dd1/search';
         $data['main'][] = $this->dofetch('Layout/Table', $data);
         $this->view('Layout/Default', $data);
     }
 
     private function Dd1Default()
     {
-        Functions::setTitle("Laporan DD1");
+        Functions::setTitle('Laporan DD1');
         $searchData = $this->Dd1SearchData();
         // TODO: Menampilkan Table
         $data = [
             'thead' => $this->my_model->getDd1Thead(),
             'data' => $searchData['data'],
-            'panjang' => $searchData['panjang']
+            'panjang' => $searchData['panjang'],
         ];
         // var_dump($this->Dd1SearchData());
         // var_dump($this->my_model->getDd1Thead());
@@ -64,7 +64,7 @@ class Laporan extends Controller
             'data' => $searchData['data'],
             'panjang' => $searchData['panjang'],
             'download' => true,
-            'format' => 'pdf'
+            'format' => 'pdf',
         ];
 
         $options = new Options();
@@ -74,7 +74,7 @@ class Laporan extends Controller
 
         $content = $this->pdfContent('Laporan/DD1', $data);
 
-        $this->downloadPdf($content, "Laporan-DD1.pdf", $options);
+        $this->downloadPdf($content, 'Laporan-DD1.pdf', $options);
     }
 
     private function Dd1DownloadXlsx()
@@ -85,9 +85,11 @@ class Laporan extends Controller
             $tags = array_map('Functions::getTags', $row);
 
             foreach ($row as $key => $value) {
-                $tag = implode("|", $tags[$key]);
+                $tag = implode('|', $tags[$key]);
                 $value = strip_tags($value);
-                if ($tag) $value .= "|{$tag}";
+                if ($tag) {
+                    $value .= "|{$tag}";
+                }
                 $row[$key] = "{$value}";
             }
 
@@ -113,7 +115,7 @@ class Laporan extends Controller
                 's' => $row['lhr'],
                 't' => $row['npk'],
                 'u' => $row['keterangan'],
-                'newline' => ''
+                'newline' => '',
             ];
         }
 
@@ -146,7 +148,7 @@ class Laporan extends Controller
             [
                 'c5' => ': ' . date('Y'),
                 'absolute' => '',
-            ]
+            ],
         );
 
         $spreadsheet = $this->spreadsheetContent($data, 12, 'Laporan/dd1.xls');
@@ -158,27 +160,35 @@ class Laporan extends Controller
     {
         $data = $this->Dd1SearchData();
         echo json_encode($data);
-        exit;
+        exit();
     }
 
     private function Dd1SearchData()
     {
         $kepemilikan_opt = $this->options('kepemilikan_opt'); // TODO: Get Kepemilikan Options
-        $list =  $this->my_model->getLaporanDd1();
+        $list = $this->my_model->getLaporanDd1();
         $data = $list;
 
         $alphabet = range('A', 'Z');
 
         $field = [];
         foreach ($this->model('Laporan_model')->getDd1Thead()[3] as $row) {
-            $row['field'] = ($row['field'] == 'perkerasan_1') ? 'perkerasan_2' : (($row['field'] == 'perkerasan_2') ? 'perkerasan_1' : $row['field']);
-            if (!empty($row['field'])) $field[$row['field']] = '';
+            $row['field'] =
+                $row['field'] == 'perkerasan_1'
+                    ? 'perkerasan_2'
+                    : ($row['field'] == 'perkerasan_2'
+                        ? 'perkerasan_1'
+                        : $row['field']);
+            if (!empty($row['field'])) {
+                $field[$row['field']] = '';
+            }
         }
 
         $start = 0;
         foreach ($list as $idx => $row) {
             if ($row['kepemilikan'] != $list[$idx - 1]['kepemilikan']) {
-                $field['nama_jalan'] = "<strong>{$alphabet[$row['kepemilikan'] - 1]}. {$kepemilikan_opt[$row['kepemilikan']]}</strong>";
+                $field['nama_jalan'] = "<strong>{$alphabet[$row['kepemilikan'] -
+                        1]}. {$kepemilikan_opt[$row['kepemilikan']]}</strong>";
                 array_splice($data, $idx + $start, 0, [$field]);
                 $start++;
             }
@@ -192,23 +202,29 @@ class Laporan extends Controller
         for ($i = 1; $i <= 4; $i++) {
             $n[$i] = 0;
         }
-        $panjang = array_merge($panjang, ["perkerasan" => $n]);
-        $panjang = array_merge($panjang, ["kondisi" => $n]);
-        $panjang = array_merge($panjang, ["kondisi_percent" => $n]);
+        $panjang = array_merge($panjang, ['perkerasan' => $n]);
+        $panjang = array_merge($panjang, ['kondisi' => $n]);
+        $panjang = array_merge($panjang, ['kondisi_percent' => $n]);
 
         foreach ($data as $row) {
             $panjang['jalan'] += $row['panjang_km'];
             foreach ($panjang as $key => $value) {
                 foreach ($value as $k => $v) {
                     if ($key != 'kondisi_percent') {
-                        $panjang[$key][$k] += number_format($row["{$key}_{$k}"], 2);
+                        $panjang[$key][$k] += number_format(
+                            $row["{$key}_{$k}"],
+                            2,
+                        );
                     }
                 }
             }
         }
 
         foreach ($panjang['kondisi_percent'] as $key => $value) {
-            $panjang['kondisi_percent'][$key] = number_format($panjang['kondisi'][$key] / $panjang['jalan'] * 100, 2);
+            $panjang['kondisi_percent'][$key] = number_format(
+                ($panjang['kondisi'][$key] / $panjang['jalan']) * 100,
+                2,
+            );
 
             if (in_array($key, [1, 2])) {
                 $panjang['mantap'] += $panjang['kondisi_percent'][$key];
@@ -221,7 +237,7 @@ class Laporan extends Controller
 
         return [
             'data' => $data,
-            'panjang' => $panjang
+            'panjang' => $panjang,
         ];
     }
 
@@ -232,7 +248,7 @@ class Laporan extends Controller
         switch ($param1) {
             case 'search':
                 $this->Dd2Search();
-                exit;
+                exit();
             default:
                 $this->Dd2Default();
                 break;
@@ -241,13 +257,13 @@ class Laporan extends Controller
 
     private function Dd2Default()
     {
-        Functions::setTitle("Laporan DD2");
+        Functions::setTitle('Laporan DD2');
 
         // TODO: Menampilkan Table
         $data['thead'] = $this->my_model->getDd2Thead();
         $data['data'] = Functions::makeTableData(['show-export' => 'true']);
         $data['search'] = false;
-        $data['url'] = BASE_URL . "/Laporan/dd2/search";
+        $data['url'] = BASE_URL . '/Laporan/dd2/search';
         $data['main'][] = $this->dofetch('Layout/Table', $data);
         $this->view('Layout/Default', $data);
     }
@@ -255,26 +271,29 @@ class Laporan extends Controller
     private function Dd2Search()
     {
         $kepemilikan_opt = $this->options('kepemilikan_opt'); // TODO: Get Kepemilikan Options
-        $list =  $this->my_model->getLaporanDd2();
+        $list = $this->my_model->getLaporanDd2();
         $data = $list;
 
         $alphabet = range('A', 'Z');
 
         $field = [];
         foreach ($this->model('Laporan_model')->getDd2Thead()[3] as $row) {
-            if (!empty($row['field'])) $field[$row['field']] = '';
+            if (!empty($row['field'])) {
+                $field[$row['field']] = '';
+            }
         }
 
         $start = 0;
         foreach ($list as $idx => $row) {
             if ($row['kepemilikan'] != $list[$idx - 1]['kepemilikan']) {
-                $field['nama_jalan'] = "<strong>{$alphabet[$row['kepemilikan'] - 1]}. {$kepemilikan_opt[$row['kepemilikan']]}</strong>";
+                $field['nama_jalan'] = "<strong>{$alphabet[$row['kepemilikan'] -
+                        1]}. {$kepemilikan_opt[$row['kepemilikan']]}</strong>";
                 array_splice($data, $idx + $start, 0, [$field]);
                 $start++;
             }
         }
 
         echo json_encode($data);
-        exit;
+        exit();
     }
 }
