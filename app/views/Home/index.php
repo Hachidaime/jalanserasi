077ba7992e82e3e68353eb7ef25c3a2e6b9edcf9
Hachidaime <cold.kid40@gmail.com>
2020-12-05 21:56:35 +0700

    Update Home: update layout

--- app/views/Home/index.php
+++ app/views/Home/index.php
@@ -1,71 +1,291 @@
+{literal}
+<style>
+  .content p {
+    text-align: justify;
+    font-size: 1rem !important;
+  }
+</style>
+{/literal}
 <div class="row">
-    <div class="col-md-4">
-        <div>
-            <p class="text-justify">
-                <b>Selamat datang</b> di website Sistem Jaringan Jalan Kabupaten Semarang, website ini didesain sebagai penguatan database dan survey kondisi jalan dalam bentuk visualisasi GIS (Geographic Information System) dalam membantu Dinas Pekerjaan Umum Kabupaten Semarang, Bidang Bina Marga sebagai Tupoksi Bidang Pembangunan, Pemeliharaan Jalan dan Jembatan.<br><br>
-				Kami sadari bahwa di dalam website ini akan ditemukan berbagai kekurangan mengingat arus informasi senantiasa bergerak dinamis. Namun kami akan senantiasa melakukan perbaikan dan pengembangan secara bertahap demi peningkatan mutu informasi yang kami sajikan. Semoga informasi yang terkadung di dalamnya memberi nilai manfaat yang nyata bagi masyarakat.<br> <br>
-                Terima kasih telah mengunjungi website kami sekaligus mengapresiasi apa yang telah kami lakukan.
-            </p>
+  <div class="col-12">
+    <div id="demo" class="carousel slide" data-ride="carousel">
+      <!-- Indicators 
+              <ul class="carousel-indicators">
+              <li data-target="#demo" data-slide-to="0" class="active"></li>
+              <li data-target="#demo" data-slide-to="1"></li>
+              <li data-target="#demo" data-slide-to="2"></li>
+              <li data-target="#demo" data-slide-to="3"></li>
+              </ul>
+              -->
+
+      <!-- The slideshow -->
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            src="{$smarty.const.SERVER_BASE}/assets/img/slide1.jpg?t={$smarty.now}"
+            alt="Bupati"
+            class="img-fluid"
+          />
         </div>
+        <div class="carousel-item">
+          <img
+            src="{$smarty.const.SERVER_BASE}/assets/img/slide2.jpg?t={$smarty.now}"
+            alt="TNI"
+            class="img-fluid"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="{$smarty.const.SERVER_BASE}/assets/img/slide3.jpg?t={$smarty.now}"
+            alt="Jalan"
+            class="img-fluid"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="{$smarty.const.SERVER_BASE}/assets/img/slide4.jpg?t={$smarty.now}"
+            alt="Gedong9"
+            class="img-fluid"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="{$smarty.const.SERVER_BASE}/assets/img/slide5.jpg?t={$smarty.now}"
+            alt="ElingBening - RawaPening"
+            class="img-fluid"
+          />
+        </div>
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
     </div>
-    <div class="col-md-8">
-        <img src="{$smarty.const.SERVER_BASE}/assets/img/bg-main.png?t={$smarty.now}" class="img-fluid mb-3" >
-    </div>
+  </div>
 </div>
 <div class="row">
-    <div class="col-md-4">
-        {foreach from=$smarty.const.SITE_INFO key=k item=v}
-        {if $k eq 'Alamat'}
-        <p class="text-justify">
-            <strong>{$k}</strong>: {$v}
+  <div class="col-lg-9 p-3">
+    <div class="row">
+      <div class="col-12 content">
+        <p>
+          Selamat datang di
+          <a class="text-danger" href="http://jalan-serasi.id/">
+            Jalan-Serasi.id
+          </a>
         </p>
-        {else}
+        <p>
+          <a class="text-danger" href="http://jalan-serasi.id/">
+            Jalan-Serasi.id
+          </a>
+          merupakan Sistem Informasi Jalan dan Jembatan Kabupaten Semarang yang
+          dikelola oleh Dinas Pekerjaan Umum Kabupaten Semarang. Website ini
+          sebagai bentuk inovasi dari konsep aksi perubahan Pemerintah Kabupaten
+          Semarang dalam mewujudkan pelayanan informasi publik yang lebih cepat
+          dan mudah diakses.
+        </p>
+        <p>
+          Informasi yang ada di
+          <a class="text-danger" href="http://jalan-serasi.id/">
+            Jalan-Serasi.id
+          </a>
+          saat ini meliputi peta jaringan jalan, data jalan dan jembatan, dan
+          pengaduan bagi masyarakat yang ingin memberikan laporan atau informasi
+          tentang kondisi jalan dan jembatan kabupaten. Selain itu, terdapat
+          menu galeri yang berisi informasi dan dokumentasi kegiatan penanganan
+          jalan dan jembatan kabupaten yang dilaksanakan oleh Bidang Bina Marga,
+          Dinas Pekerjaan Umum Kabupaten Semarang.
+        </p>
+        <p>
+          Kami menyadari bahwa website ini masih banyak kekurangan mengingat
+          arus informasi senantiasa bergerak dinamis. Namun kami akan senantiasa
+          melakukan perbaikan dan pengembangan secara bertahap demi peningkatan
+          mutu informasi yang kami sajikan. Semoga informasi yang terkadung di
+          dalamnya memberi nilai manfaat yang nyata bagi masyarakat.
+        </p>
+        <p>
+          kasih telah mengunjungi website kami sekaligus mengapresiasi apa yang
+          telah kami lakukan.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
         <div class="row">
-            <div class="col-md-3 pr-0"><strong>{$k}<span class="float-lg-right float-md-right float-sm-none">:</span></strong></div>
-            <div class="col-md-9">{$v}</div>
+          <div class="col-12">PEKERJAAN UMUM KABUPATEN SEMARANG</div>
         </div>
-        {/if}
-        {/foreach}
-    </div>
-    <div class="col-md-8">
-        <div id="demo" class="carousel slide" data-ride="carousel">
-
-            <!-- Indicators 
-            <ul class="carousel-indicators">
-            <li data-target="#demo" data-slide-to="0" class="active"></li>
-            <li data-target="#demo" data-slide-to="1"></li>
-            <li data-target="#demo" data-slide-to="2"></li>
-            <li data-target="#demo" data-slide-to="3"></li>
-            </ul>
-            -->
-
-            <!-- The slideshow -->
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="{$smarty.const.SERVER_BASE}/assets/img/Slide1.png?t={$smarty.now}" alt="Bupati" class="img-fluid">
-                </div>
-                <div class="carousel-item">
-                    <img src="{$smarty.const.SERVER_BASE}/assets/img/Slide2.png?t={$smarty.now}" alt="TNI" class="img-fluid">
-                </div>
-                <div class="carousel-item">
-                    <img src="{$smarty.const.SERVER_BASE}/assets/img/Slide3.png?t={$smarty.now}" alt="Jalan" class="img-fluid">
-                </div>
-                <div class="carousel-item">
-                    <img src="{$smarty.const.SERVER_BASE}/assets/img/Slide4.png?t={$smarty.now}" alt="Gedong9" class="img-fluid">
-                </div>
-				<div class="carousel-item">
-                    <img src="{$smarty.const.SERVER_BASE}/assets/img/Slide5.png?t={$smarty.now}" alt="ElingBening - RawaPening" class="img-fluid">
-                </div>
-            </div>
-
-            <!-- Left and right controls -->
-            <a class="carousel-control-prev" href="#demo" data-slide="prev">
-                <span class="carousel-control-prev-icon"></span>
-            </a>
-            <a class="carousel-control-next" href="#demo" data-slide="next">
-                <span class="carousel-control-next-icon"></span>
-            </a>
+        <div class="row">
+          <div class="col-lg-2 col-3">Alamat :</div>
+          <div class="col-lg-10 col-9">Jl. KH. Hasyim Ashari No. 3 Ungaran</div>
         </div>
-        <!-- <img src="https://dummyimage.com/1024x200/000/fff.png&text=Lorem+ipsum" class="img-fluid" > -->
+        <div class="row">
+          <div class="col-lg-2 col-3">Telepon :</div>
+          <div class="col-lg-10 col-9">(024) 6921607</div>
+        </div>
+        <div class="row">
+          <div class="col-lg-2 col-3">Email :</div>
+          <div class="col-lg-10 col-9">binamarga.semarangkab@gmail.com</div>
+        </div>
+      </div>
     </div>
+  </div>
+  <div
+    class="col-lg-3 p-3 d-flex flex-column flex-lg-column flex-md-row align-items-center"
+  >
+    <div class="card m-2" style="width: 200px">
+      <img
+        src="{$smarty.const.SERVER_BASE}/assets/img/avatar1.png?t={$smarty.now}"
+        class="card-img-top"
+        alt="Kepala Dinas"
+        style="
+          width: 200px;
+          height: 300px;
+          object-fit: cover;
+          object-position: center;
+        "
+      />
+      <div class="card-body p-2 text-center">
+        <h5 class="card-title">Kepala Dinas PUPR</h5>
+        <p class="card-text">Lorem, ipsum dolor.</p>
+      </div>
+    </div>
+    <div class="card m-2" style="width: 200px">
+      <img
+        src="{$smarty.const.SERVER_BASE}/assets/img/avatar2.png?t={$smarty.now}"
+        class="card-img-top"
+        alt="Kepala Bidang"
+        style="
+          width: 200px;
+          height: 300px;
+          object-fit: cover;
+          object-position: center;
+        "
+      />
+      <div class="card-body p-2 text-center">
+        <h5 class="card-title">Kepala Bidang<br />Bina Marga</h5>
+        <p class="card-text">Lorem, ipsum dolor.</p>
+      </div>
+    </div>
+    <div class="text-center m-2" style="width: 250px">
+      <p>Link :</p>
+      <a class="text-danger" href="http://dpu.semarang kota.go.id/">
+        http://dpu.semarang kota.go.id/
+      </a>
+      <a class="text-danger" href="http://jalanpu.semar angkota.go.id/">
+        http://jalanpu.semar angkota.go.id/
+      </a>
+    </div>
+  </div>
 </div>
+{*
+<div class="row">
+  <div class="col-md-4">
+    <div>
+      <p class="text-justify">
+        <b>Selamat datang</b> di website Sistem Jaringan Jalan Kabupaten
+        Semarang, website ini didesain sebagai penguatan database dan survey
+        kondisi jalan dalam bentuk visualisasi GIS (Geographic Information
+        System) dalam membantu Dinas Pekerjaan Umum Kabupaten Semarang, Bidang
+        Bina Marga sebagai Tupoksi Bidang Pembangunan, Pemeliharaan Jalan dan
+        Jembatan.<br /><br />
+        Kami sadari bahwa di dalam website ini akan ditemukan berbagai
+        kekurangan mengingat arus informasi senantiasa bergerak dinamis. Namun
+        kami akan senantiasa melakukan perbaikan dan pengembangan secara
+        bertahap demi peningkatan mutu informasi yang kami sajikan. Semoga
+        informasi yang terkadung di dalamnya memberi nilai manfaat yang nyata
+        bagi masyarakat.<br />
+        <br />
+        Terima kasih telah mengunjungi website kami sekaligus mengapresiasi apa
+        yang telah kami lakukan.
+      </p>
+    </div>
+  </div>
+  <div class="col-md-8">
+    <img
+      src="{$smarty.const.SERVER_BASE}/assets/img/bg-main.png?t={$smarty.now}"
+      class="img-fluid mb-3"
+    />
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-4">
+    {foreach from=$smarty.const.SITE_INFO key=k item=v} {if $k eq 'Alamat'}
+    <p class="text-justify"><strong>{$k}</strong>: {$v}</p>
+    {else}
+    <div class="row">
+      <div class="col-md-3 pr-0">
+        <strong
+          >{$k}<span class="float-lg-right float-md-right float-sm-none"
+            >:</span
+          ></strong
+        >
+      </div>
+      <div class="col-md-9">{$v}</div>
+    </div>
+    {/if} {/foreach}
+  </div>
+  <div class="col-md-8">
+    <div id="demo" class="carousel slide" data-ride="carousel">
+      <!-- Indicators 
+              <ul class="carousel-indicators">
+              <li data-target="#demo" data-slide-to="0" class="active"></li>
+              <li data-target="#demo" data-slide-to="1"></li>
+              <li data-target="#demo" data-slide-to="2"></li>
+              <li data-target="#demo" data-slide-to="3"></li>
+              </ul>
+              -->
+
+      <!-- The slideshow -->
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            src="{$smarty.const.SERVER_BASE}/assets/img/Slide1.png?t={$smarty.now}"
+            alt="Bupati"
+            class="img-fluid"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="{$smarty.const.SERVER_BASE}/assets/img/Slide2.png?t={$smarty.now}"
+            alt="TNI"
+            class="img-fluid"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="{$smarty.const.SERVER_BASE}/assets/img/Slide3.png?t={$smarty.now}"
+            alt="Jalan"
+            class="img-fluid"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="{$smarty.const.SERVER_BASE}/assets/img/Slide4.png?t={$smarty.now}"
+            alt="Gedong9"
+            class="img-fluid"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="{$smarty.const.SERVER_BASE}/assets/img/Slide5.png?t={$smarty.now}"
+            alt="ElingBening - RawaPening"
+            class="img-fluid"
+          />
+        </div>
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
+    </div>
+    <!-- <img src="https://dummyimage.com/1024x200/000/fff.png&text=Lorem+ipsum" class="img-fluid" > -->
+  </div>
+</div>
+*}
